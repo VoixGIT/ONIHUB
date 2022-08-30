1050a52b59ceb12f001b9e427c193eeaef0c5c31
VoixGIT <95440864+VoixGIT@users.noreply.github.com>
2022-08-30 15:08:19 +0100

    Update EMOJI.lua

--- EMOJI.lua
+++ EMOJI.lua
@@ -1,18 +1,1 @@
 {"clown":"🤡","lol":"🤣","LOL":"🤣","think":"🤔","THINK":"🤔","kiss":"😘","KISS":"😘","LOVE":"🥰","love":"🥰","heart":"❤️","HEART":"❤️","pray":"🙏","PRAY":"🙏","smug":"😏","SMUG":"😏","eyes":"👀","EYES":"👀","sweat":"🥵","SWEAT":"🥵","uk":"🇬🇧","CAM":"📸","cam":"📸","AYO":"AYO🤨📸","ayo":"AYO🤨📸","SUS":"🤨","sus":"🤨","KEK":"😂","kek":"😂","SKULL":"💀","skull":"💀","bruh":"😶","BRUH":"😶","UK":"🇬🇧"}
-
--- 🤡 :clown:
--- 🤣 :lol: + :LOL:
--- 😘 :kiss: + :KISS:
--- 🥰 :love: + :LOVE:
--- ❤️ :heart: + :HEART:
--- 🙏 :pray: + :PRAY:
--- 😏 :smug: + :SMUG:
--- 👀 :eyes: + :EYES:
--- 🥵 :sweat: + :SWEAT:
--- 🇬🇧 :uk:
--- 📸 :cam: + :CAM:
--- AYO🤨📸 :AYO: + :ayo:
--- 🤨 :sus: + :SUS:
--- 😂 :kek: + :KEK:
--- 💀 :skull: + :SKULL:
--- 😶 :bruh: + :BRUH:
